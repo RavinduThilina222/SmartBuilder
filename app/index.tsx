253efa75db8f1a8d793c412916119f50d37ea19e
kethsiya580 <161032408+kethsiya580@users.noreply.github.com>
2025-01-19 23:28:44 +0530

    Add files via upload

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,16 @@ import Welcome from './(pages)/Welcome';
 import Login from './(pages)/Login';
 import Signup from './(pages)/signup';
 import MaterialScreenAdmin from './(pages)/MaterialScreenAdmin';
-import AddMaterial from './(pages)/AddMaterial';
+import Labor from './(pages)/Labor';
+import LabourList from './(pages)/LabourList';
+import AdminLabor from './(pages)/AdminLabor';
+import HireLabor from './(pages)/HireLabor';
+import Addlabor from './(pages)/AddLabor';
+import ViewLabor from './(pages)/ViewLabor';
+import UpdateLabor from './(pages)/UpdateLabor';
+import DeleteLabor from './(pages)/DeleteLabor';
+import LaborDetail from './(pages)/LaborDetail';  
+
 
 const Stack = createStackNavigator();
 
@@ -15,7 +24,15 @@ export default function Index() {
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name='signup' component={Signup} />
       <Stack.Screen name='MaterialScreenAdmin' component={MaterialScreenAdmin} />
-      <Stack.Screen name='AddMaterial' component={AddMaterial} />
+      <Stack.Screen name='Labor' component={Labor} />
+      <Stack.Screen name='LabourList' component={LabourList} />
+      <Stack.Screen name='AdminLabor' component={AdminLabor} />
+      <Stack.Screen name='HireLabor' component={HireLabor} />
+      <Stack.Screen name='Addlabor' component={Addlabor} />
+      <Stack.Screen name='ViewLabor' component={ViewLabor} />
+      <Stack.Screen name='UpdateLabor' component={UpdateLabor} />
+      <Stack.Screen name='DeleteLabor' component={DeleteLabor} />
+      <Stack.Screen name='LaborDetail' component={LaborDetail} />
     </Stack.Navigator>
   );
 }
